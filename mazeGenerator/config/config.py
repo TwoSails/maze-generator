1bf9e6d86c7732d706819fa35aae732eaae7b9af
TwoSails <davidwoolgrove@icloud.com>
2022-11-03 09:56:07 +0000

    Implemented all methods for config class and implemented all tests for config

--- mazeGenerator/config/config.py
+++ mazeGenerator/config/config.py
@@ -1,7 +1,8 @@
 """
 File: config.py
 Date Created: 31/10/22
-Description: This is reference to load in the configuration file which has default program parameters such as the path which the tiles are located
+Description: This is reference to load in the configuration file which has
+default program parameters such as the path which the tiles are located
 """
 
 import json
@@ -11,14 +12,61 @@ class Config:
     def __init__(self) -> None:
         self.loadedConfig = False
         self.__configPath: str = "mazeGenerator/config/config.json"
+        self.__configValues = []
         self.loadConfig()
 
     def loadConfig(self):
+        """
+        This method loads the configuration file,
+        and sets the keys as attributes for dot notation access
+        :return:
+        """
         try:
             with open(self.__configPath, 'r') as file:
                 content = json.load(file)
                 for key in content.keys():
                     setattr(self, key, content[key])
+                    self.__configValues.append(key)
                 self.loadedConfig = True
         except FileNotFoundError:
             pass
+
+    def saveConfig(self):
+        config = {}
+        for key in self.__configValues:
+            config[key] = getattr(self, key)
+
+        with open(self.__configPath, "w") as file:
+            json.dump(config, file, indent=4)
+
+    def get(self, key: str) -> any:
+        """
+        Fetching method for parameters
+        :param key: identifier for value being fetched
+        :return: attribute fetched
+        """
+        if key in self.__configValues:
+            return getattr(self, key)
+        else:
+            return None
+
+    def set(self, key: str, value, save: bool = False) -> bool:
+        """
+        Setter method for attributes in the config
+        :param key: identifier for value being updated/set
+        :param value: data being saved into the attribute
+        :param save: boolean to check if config file should be updated
+        :return: Success or Fail
+        """
+        if not isinstance(key, str):
+            return False
+
+        setattr(self, key, value)
+
+        if key not in self.__configValues:
+            self.__configValues.append(key)
+
+        if save:
+            self.saveConfig()
+
+        return True
--- tests/test_config.py
+++ tests/test_config.py
@@ -12,6 +12,75 @@ class TestConfig(unittest.TestCase):
         config: Config = Config()
         self.assertTrue(config.loadedConfig)
 
+    def test_get_valid_attribute(self):
+        """
+        Tests that attributes can be fetched from config file
+        :return:
+        """
+
+        config: Config = Config()
+        self.assertNotEqual(config.get("dataPath"), None)
+
+    def test_get_invalid_attribute(self):
+        """
+        Tests that no fatal error occurs when fetching invalid attribute from config file
+        :return:
+        """
+        config: Config = Config()
+        self.assertEqual(config.get("pineapplesOnPizza"), None)
+
+    def test_get_int_attribute(self):
+        """
+        Tests that no fatal error occurs when fetching integer attribute from config file
+        :return:
+        """
+        config: Config = Config()
+        self.assertEqual(config.get(1), None)
+
+    def test_set_valid_attribute(self):
+        """
+        Tests that value can be set and saved
+        :return:
+        """
+        config: Config = Config()
+        key = "alpha"
+        value = "beta"
+        config.set(key, value)
+        self.assertEqual(config.get(key), value)
+
+    def test_set_invalid_attribute(self):
+        """
+        Tests that no fatal error occurs when setting invalid attribute
+        :return:
+        """
+        config: Config = Config()
+        key = 1
+        value = "beta"
+        self.assertFalse(config.set(key, value))
+        self.assertEqual(config.get(key), None)
+
+    def test_set_valid_save_attribute(self):
+        """
+        Tests that value can be set and saved in config file
+        :return:
+        """
+        config: Config = Config()
+        key = "alpha"
+        value = "beta"
+        self.assertTrue(config.set(key, value, True))
+        self.assertEqual(config.get(key), value)
+
+    def test_set_invalid_save_attribute(self):
+        """
+        Tests that no fatal error occurs when setting invalid attribute and trying to save
+        :return:
+        """
+        config: Config = Config()
+        key = 1
+        value = "beta"
+        self.assertFalse(config.set(key, value, True))
+        self.assertEqual(config.get(key), None)
+
 
 if __name__ == "__main__":
     unittest.main()
